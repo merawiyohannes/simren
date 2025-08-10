3145c2fbc650278df2ea5beaa26499cfd86d0349
merawi <merawiyohannes@gmail.com>
2025-08-10 03:55:26 -0700

    media files modified

--- simren/settings.py
+++ simren/settings.py
@@ -15,7 +15,13 @@ CHAPA_SECRET_KEY = os.getenv('CHAPA_SECRET_KEY')
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv("CLOUD_NAME"),
     'API_KEY': os.getenv("API_KEY"),
-    'API_SECRET': os.getenv("API_SECRET")
+    'API_SECRET': os.getenv("API_SECRET"),
+    'SECURE': True,
+    'DEFAULT_TAGS': ['django-upload'],
+    'UPLOAD_OPTIONS': {
+        'resource_type': 'image',
+        'type': 'upload'  # ensures it's public
+    }
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
